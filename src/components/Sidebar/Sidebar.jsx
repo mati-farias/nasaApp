8bbf405b7c38bf097c4575f0eecc6c30d4e37e45
mati-farias <mati.farias91@hotmail.com>
2022-11-07 14:54:34 -0300

    updating design

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,101 @@
+import { Row, Col, Card, CardGroup, Container, Button } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import DatePicker from 'react-datepicker';
+import { getRoversPics } from '../../redux/actions';
+import { useParams } from 'react-router-dom';
+import FilterCamera from '../FilterCamera/FilterCamera';
+function Sidebar(props) {
+	const { pagination } = props;
+	const dispatch = useDispatch();
+	const { rover } = useParams();
+
+	const [date, setDate] = useState(new Date());
+	const [dateType, setDateType] = useState(true);
+	const [input, setInput] = useState(1000);
+	useEffect(() => {
+		dispatch(getRoversPics(new Date(), rover));
+	}, [dispatch]);
+
+	function handleDate(date) {
+		console.log('handleDate', date);
+		setDate(date);
+		dispatch(getRoversPics(date, rover));
+	}
+
+	function handleSetDataType() {
+		setDateType(!dateType);
+		console.log(typeof roverPics[0]?.earth_date);
+		setDate('');
+	}
+	return (
+		<Container
+			style={{
+				minHeight: '100vh',
+				padding: '2rem',
+				backgroundColor: '#000000AA',
+			}}>
+			<Col
+				style={{
+					justifyContent: 'space-around',
+					minHeight: '100vh',
+					alignItems: 'center',
+					display: 'flex',
+					flexDirection: 'column',
+				}}>
+				<Row>
+					<h1
+						style={{
+							justifyContent: 'center',
+							color: '#ff52349f',
+							fontFamily: 'Dancing Script',
+						}}>
+						{rover.toUpperCase()}
+					</h1>
+				</Row>
+				<Row>
+					{dateType ? (
+						<Col>
+							<DatePicker
+								dateFormat='yyyy-MM-dd'
+								selected={date}
+								value={date}
+								onChange={(e) => handleDate(e)}
+								className='form-control w-100'
+								placeholder='Pick a date!'
+								inline
+							/>
+						</Col>
+					) : (
+						<Col style={{ paddingBottom: '170px' }}>
+							<input
+								className='w-100'
+								type='text'
+								value={input}
+								onChange={(e) => setInput(e.target.value)}
+							/>
+							<Button
+								style={{ marginTop: '10px' }}
+								onClick={() => handleDate(input)}>
+								Search
+							</Button>
+						</Col>
+					)}
+				</Row>
+				<Row>
+					<Button
+						className='w-70 btn btn-primary'
+						type='button'
+						onClick={handleSetDataType}>
+						{dateType ? 'Earth Date' : 'Sol Date'}
+					</Button>
+				</Row>
+				<Row>
+					<FilterCamera pagination={pagination} />
+				</Row>
+			</Col>
+		</Container>
+	);
+}
+
+export default Sidebar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,16 +11,11 @@ import FilterCamera from '../../components/FilterCamera/FilterCamera';
 import { Row, Col, Card, CardGroup, Container, Button } from 'react-bootstrap';
 import backgroundImage from '../../images/marsbackground.jpg';
 import Loader from '../../components/Loader/Loader';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 const Home = (props) => {
-	const dispatch = useDispatch();
-	const { rover } = useParams();
 	const roverPics = useSelector((state) => state.roverData);
 	const loading = useSelector((state) => state.loading);
-
-	const [date, setDate] = useState(new Date());
-	const [dateType, setDateType] = useState(true);
-	const [input, setInput] = useState(1000);
 
 	//Pagination
 	const [currentPage, setCurrentPage] = useState(1);
@@ -31,22 +26,6 @@ const Home = (props) => {
 	const pagination = (pageNumber) => {
 		setCurrentPage(pageNumber);
 	};
-
-	useEffect(() => {
-		dispatch(getRoversPics(new Date(), rover));
-	}, [dispatch]);
-
-	function handleDate(date) {
-		console.log('handleDate', date);
-		setDate(date);
-		dispatch(getRoversPics(date, rover));
-	}
-
-	function handleSetDataType() {
-		setDateType(!dateType);
-		console.log(typeof roverPics[0]?.earth_date);
-		setDate('');
-	}
 
 	return (
 		<Container
@@ -62,90 +41,34 @@ const Home = (props) => {
 				backgroundSize: 'cover',
 				backgroundAttachment: 'fixed',
 			}}>
-			<Container
-				style={{
-					minHeight: '10vh',
-					padding: '2rem',
-				}}>
-				<Col>
-					<h1
-						style={{
-							justifyContent: 'center',
-							color: '#ff52349f',
-							fontFamily: 'Dancing Script',
-						}}>
-						{rover.toUpperCase()}
-					</h1>
-				</Col>
+			<Container fluid>
 				<Row>
-					<Col>
-						{dateType ? (
-							<Col>
-								<DatePicker
-									dateFormat='yyyy-MM-dd'
-									selected={date}
-									value={date}
-									onChange={(e) => handleDate(e)}
-									className='form-control w-100'
-									placeholder='Pick a date!'
-									inline
-								/>
-							</Col>
-						) : (
-							<Col style={{ paddingBottom: '170px' }}>
-								<input
-									className='w-100'
-									type='text'
-									value={input}
-									onChange={(e) => setInput(e.target.value)}
-								/>
-								<Button
-									style={{
-										marginTop: '10px',
-										color: '#ff52349f',
-									}}
-									onClick={() => handleDate(input)}>
-									Search
-								</Button>
-							</Col>
-						)}
+					<Col xs={2}>
+						<Sidebar pagination={pagination} />
 					</Col>
-					<Col>
-						<FilterCamera pagination={pagination} />
+					<Col xs={10}>
+						<Col className='d-flex justify-content-center'>
+							<Pagination
+								photosPerPage={photosPerPage}
+								roverPics={roverPics.length}
+								pagination={pagination}
+								currentPage={currentPage}
+							/>
+						</Col>
+						<div>
+							{loading ? (
+								<Loader />
+							) : photosToShow.length > 0 ? (
+								<PhotoList photosToShow={photosToShow} />
+							) : (
+								<h2>
+									No photos found. Try another camera or
+									another date!
+								</h2>
+							)}
+						</div>
 					</Col>
-					<Button
-						style={{
-							backgroundColor: '#0000009f',
-							color: '#ff52349f',
-							border: 'none',
-						}}
-						className='w-70 btn btn-primary'
-						type='button'
-						onClick={handleSetDataType}>
-						{dateType ? 'Earth Date' : 'Sol Date'}
-					</Button>
 				</Row>
-			</Container>
-			<Container>
-				<Col className='d-flex justify-content-center'>
-					<Pagination
-						photosPerPage={photosPerPage}
-						roverPics={roverPics.length}
-						pagination={pagination}
-						currentPage={currentPage}
-					/>
-				</Col>
-				<div>
-					{loading ? (
-						<Loader />
-					) : photosToShow.length > 0 ? (
-						<PhotoList photosToShow={photosToShow} />
-					) : (
-						<h2>
-							No photos found. Try another camera or another date!
-						</h2>
-					)}
-				</div>
 			</Container>
 		</Container>
 	);
